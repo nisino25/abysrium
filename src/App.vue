7e6e18b6da8343384679854eee79274dc0d0f124
nisino25 <nisino25@gmail.com>
2021-08-29 04:13:37 +0900

    added local storage

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <template>
-  <div class="timer">
+  <div>
+    <div class="timer">
     <h3>Timer</h3>
 
     <div class="container">
@@ -87,11 +88,11 @@
   
   <hr>
 
-  <div class="to-do-lis">
+  <div>
 
     <div class="navbar">
       <ul>
-        <li @click="showingPage= 'Abyss'" :class="[showingPage === 'Abyss' ? 'Abyss' : '']"><a >Abyss</a></li>
+        <li @click="showingPage= 'Abyss'" :class="[showingPage === 'Abyss' ? 'Abyss' : '']" ><a >Abyss</a></li>
         <li @click="showingPage= 'Fresh' " :class="[showingPage === 'Fresh' ? 'Fresh' : '']" ><a >Fresh</a></li>
         <li @click="showingPage= 'Hallow'" :class="[showingPage === 'Hallow' ? 'Hallow' : '']"><a >Halloween</a></li>
         <li @click="showingPage= 'Anni'" :class="[showingPage === 'Anni' ? 'Anni' : '']"><a >Annivdersary</a></li>
@@ -102,7 +103,47 @@
 
     </div>
 
+    <div class="to-do-list" >
+      <h4>To-do-list</h4>
+      <!-- <button @click="openingModal = true">Add task</button> -->
+
+      <div id="myDIV" class="header " :style="bgc">
+        <h2 style="margin:5px">My To Do List</h2>
+        <input type="text" id="myInput" placeholder="Reminde Title.." v-model="inputText">
+        <span @click="AddTask(showingPage)" class="addBtn">Add</span>
+        <input type="number" id="myInput" placeholder="Times..." v-model="inputNum">
+        <span  class="addBtn">Times</span>
+        <!-- <span  class="addBtn" @click="test('Abyss')">Times</span> -->
+        
+      </div>
+
+      <ul id="myUL" v-for="(task, i) in TaskList.[showingPage]" :key="i">
+
+        <div v-if="task.showing">
+          <li @click="task.currentNum++" v-if="task.currentNum < task.goalNum">{{task.title}}: {{task.currentNum}}/{{task.goalNum}}</li>
+          <li @click="deleteTask(showingPage, i)" v-else style="background: #888">{{task.title}}: {{task.currentNum}}/{{task.goalNum}}</li>
+        </div>
+        
+        
+      </ul>
+
+
+
+      <div class="abyss">
+
+        
+      </div>
+
+
+    </div>
+
   </div>
+
+  </div>
+
+
+  
+
 
   
   
@@ -122,7 +163,13 @@ export default {
       },
 
       showingPage: 'Abyss',
+      
+      inputNum:1,
+      inputText: '',
 
+      bgc: 'background-color: 	dodgerblue;',
+
+      TaskList:{Abyss:[], Fresh:[], Hallow:[], Anni:[], Chris:[], pubg:[]},
       
 
       remaining: null,
@@ -165,6 +212,28 @@ export default {
 
   methods:{
 
+    AddTask(name){
+      if(this.inputNum == 0 || this.inputText == ''){
+        return
+      }
+      // this.results.push({time: Date.now(),outcome: Number(this.currentTime) });
+      this.TaskList.[name].push({goalNum: this.inputNum, currentNum: 0, title: this.inputText,finishd: false,showing: true});
+      console.log(this.TaskList.[name])
+
+      this.inputNum = 1;
+      this.inputText = '';
+    },
+
+    deleteTask(name,i){
+      let r= confirm(`Deleting [${this.TaskList.[name].[i].title}]...`);
+      if(r){
+        this.TaskList.[name].[i].showing = false;
+        return;
+      }
+    },
+
+
+
     count1: function() {
       if (this.sec1 <= 0 && this.min1 >= 1) {
         this.min1 --;
@@ -175,24 +244,20 @@ export default {
         this.sec1 --;
       }
     },
-
     start1: function() {
       let self = this;
       this.timer1Obj = setInterval(function() {self.count1()}, 1000)
       this.timer1On = true
     },
-
     stop1: function() {
       clearInterval(this.timer1Obj);
       this.timer1On = false
       this.borderColor1 = 'solid 3px black';
     },
-
     complete1: function() {
       this.borderColor1 = 'solid 3px red';
       clearInterval(this.timer1Obj)
     },
-
 
 
     count2: function() {
@@ -205,24 +270,20 @@ export default {
         this.sec2 --;
       }
     },
-
     start2: function() {
       let self = this;
       this.timer2Obj = setInterval(function() {self.count2()}, 1000)
       this.timer2On = true
     },
-
     stop2: function() {
       clearInterval(this.timer2Obj);
       this.timer2On = false
       this.borderColor2 = 'solid 3px black';
     },
-
     complete2: function() {
       this.borderColor2 = 'solid 3px red';
       clearInterval(this.timer2Obj)
     },
-
 
 
     count3: function() {
@@ -235,24 +296,20 @@ export default {
         this.sec3 --;
       }
     },
-
     start3: function() {
       let self = this;
       this.timer3Obj = setInterval(function() {self.count3()}, 1000)
       this.timer3On = true
     },
-
     stop3: function() {
       clearInterval(this.timer3Obj);
       this.timer3On = false
       this.borderColor3 = 'solid 3px black';
     },
-
     complete3: function() {
       this.borderColor1 = 'solid 3px red';
       clearInterval(this.timer3Obj)
     },
-  
   
   },
 
@@ -304,8 +361,72 @@ export default {
     },
 
 
-  }
+  },
+
+  watch:{
+
+    showingPage: function(){
+      switch (this.showingPage) {
+
+        case 'Abyss':
+          this.bgc = 'background-color: 	dodgerblue';
+          break;
+
+        case 'Fresh':
+          this.bgc = 'background-color: mediumaquamarine';
+          break;
+
+        case 'Hallow':
+          this.bgc = 'background-color: 	slateblue'
+          break;
+
+        case 'Anni':
+          this.bgc = 'background-color: 	lightskyblue';
+          break;
+
+        case 'Chris':
+          this.bgc = 'background-color: 	crimson';
+          break;
+
+        case 'pubg':
+          this.bgc = 'background-color: 	coral';
+          break;
+        
+
+        
+
+        case 'Papayas':
+          console.log('Mangoes and papayas are $2.79 a pound.');
+          // expected output: "Mangoes and papayas are $2.79 a pound."
+          break;
+
+        default:
+          alert('wff is the input')
+      }
+
+    },
+
+    TaskList: {
+      deep:true,
+      handler() {
+        localStorage.TaskList = JSON.stringify(this.TaskList); 
+        console.log('touched lsit')
+      },
+    }
+
+  },
+
+  mounted() {
+    if (localStorage.TaskList) {
+      this.TaskList = JSON.parse(localStorage.TaskList);
+      console.log('getting data')
+    }else{
+      console.log('nothugn here')
+    }
+  },
+      
 }
+
 </script>
 
 <style>
@@ -373,29 +494,156 @@ export default {
   background-color: #111;
 } */
 
-.navbar .Abyss {
+.navbar .Abyss, .Abyss {
   background-color: 	dodgerblue;
 }
 
-.navbar .Fresh {
+.navbar .Fresh, .Fresh {
   background-color: 	mediumaquamarine;
 }
 
-.navbar .Hallow {
+.navbar .Hallow, .Hallow {
   background-color: 	slateblue;
 }
 
-.navbar .Anni {
+.navbar .Anni, .Anni {
   background-color: 	lightskyblue;
 }
 
-.navbar .Chris {
+.navbar .Chris, .Chris{
   background-color: 	crimson;
 }
 
-.navbar .pubg {
+.navbar .pubg, .pubg {
   background-color: 	coral;
 }
+
+
+/* ---------- */
+
+body {
+  margin: 0;
+  min-width: 250px;
+}
+
+/* Include the padding and border in an element's total width and height */
+* {
+  box-sizing: border-box;
+}
+
+/* Remove margins and padding from the list */
+.to-do-list ul {
+  margin: 0;
+  padding: 0;
+}
+
+/* Style the list items */
+.to-do-list ul li {
+  cursor: pointer;
+  position: relative;
+  padding: 12px 8px 12px 40px;
+  list-style-type: none;
+  background: #eee;
+  font-size: 18px;
+  transition: 0.2s;
+  
+  /* make the list items unselectable */
+  -webkit-user-select: none;
+  -moz-user-select: none;
+  -ms-user-select: none;
+  user-select: none;
+}
+
+/* Set all odd list items to a different color (zebra-stripes) */
+.to-do-list ul li:nth-child(odd) {
+  background: #f9f9f9;
+}
+
+/* Darker background-color on hover */
+.to-do-list ul li:hover {
+  background: #ddd;
+}
+
+/* When clicked on, add a background color and strike out text */
+.to-do-list ul li.checked {
+  background: #888;
+  color: #fff;
+  text-decoration: line-through;
+}
+
+/* Add a "checked" mark when clicked on */
+.to-do-list ul li.checked::before {
+  content: '';
+  position: absolute;
+  border-color: #fff;
+  border-style: solid;
+  border-width: 0 2px 2px 0;
+  top: 10px;
+  left: 16px;
+  transform: rotate(45deg);
+  height: 15px;
+  width: 7px;
+}
+
+/* Style the close button */
+.close {
+  position: absolute;
+  right: 0;
+  top: 0;
+  padding: 12px 16px 12px 16px;
+}
+
+.close:hover {
+  background-color: #f44336;
+  color: white;
+}
+
+/* Style the header */
+.header {
+  background-color: ;
+  padding: 30px 40px;
+  color: white;
+  text-align: center;
+}
+
+/* Clear floats after the header */
+.header:after {
+  content: "";
+  display: table;
+  clear: both;
+}
+
+/* Style the input */
+.to-do-list input {
+  margin: 0;
+  border: none;
+  border-radius: 0;
+  width: 75%;
+  padding: 10px;
+  float: left;
+  font-size: 16px;
+}
+
+/* Style the "Add" button */
+.addBtn {
+  padding: 10px;
+  width: 25%;
+  background: #d9d9d9;
+  color: #555;
+  float: left;
+  text-align: center;
+  font-size: 16px;
+  cursor: pointer;
+  transition: 0.3s;
+  border-radius: 0;
+}
+
+.addBtn:hover {
+  background-color: #bbb;
+}
+
+
+
 
 
 
